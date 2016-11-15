371e80098464bede8ba71fa8a2c389b3116957ce
ArnaudBan <abanvillet@studio-goliath.com>
2016-11-15 11:17:13 +0100

    First commit, add the controle and the filter

---
+++ fallback-thumbnail.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Fallback thumbanil
+Description: Set a default fallback thumbnail. Your posts will always have a thumbnail.
+Version: 1.0
+Author: Studio Goliath
+Author URI: http://www.studio-goliath.com/
+*/
+
+
+add_filter( 'post_thumbnail_html', 'gft_default_post_thumbnail_html', 10, 5 );
+
+function gft_default_post_thumbnail_html( $html, $post_ID, $post_thumbnail_id, $size, $attr ){
+
+    if(  $html == '' ){
+
+        // class = wp-post-image
+        $default_image_id = get_theme_mod( 'gft_fallback_thumbnail' );
+
+        if( $default_image_id ){
+
+            $size_class = $size;
+            if ( is_array( $size_class ) ) {
+                $size_class = join( 'x', $size_class );
+            }
+            $size_class .= "attachment-$size_class size-$size_class wp-post-image";
+
+            $html = wp_get_attachment_image( $default_image_id, $size, false, array( 'class' => $size_class) );
+        }
+
+
+    }
+
+    return $html;
+}
+
+
+add_action( 'customize_register', 'gft_customize_register_default_thumbnail' );
+
+function gft_customize_register_default_thumbnail( $wp_customize ){
+
+    $wp_customize->add_setting( 'gft_fallback_thumbnail', array(
+        'type'              => 'theme_mod',
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+    ) );
+
+
+    // On ajoute le controle pour le texte en footer
+    $wp_customize->add_control( new WP_Customize_Media_Control(
+        $wp_customize,
+        'gft_fallback_thumbnail',
+        array(
+            'label'      => __('Fallback thumbnail'),
+            'section'    => 'gft_fallback_thumbnail_section',
+            'settings'   => 'gft_fallback_thumbnail',
+        )
+    ) );
+
+    // On ajout une section pour les pages spéciales
+    $wp_customize->add_section( 'gft_fallback_thumbnail_section', array(
+        'title'         => __('Fallback thumbnail'),
+        'description'   => __('Set a default fallback thumbnail'),
+        'capability'    => 'edit_theme_options',
+    ) );
+}
+
